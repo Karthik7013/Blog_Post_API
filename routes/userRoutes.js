49e92a86b02b17ae85c9974d351852577947e54b
karthik <karthiktumala143@gmail.com>
2024-06-03 22:12:01 +0530

    darkmodeController

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,11 +8,12 @@ import {
 } from "../controller/userController.js";
 import { savePost } from "../controller/postController.js";
 import isAuthenticate from "../middlewares/auth.js";
+import user from "../model/userModel.js";
 
 const userRouter = Router()
 userRouter.post("/register", userRegister);
 userRouter.post("/login", userLogin);
-userRouter.post("/rest/password",isAuthenticate, updatePassword);
+userRouter.post("/rest/password", isAuthenticate, updatePassword);
 userRouter.post("/forgot/password", forgotPassword);
 
 userRouter.get("/profile", isAuthenticate, userProfile);
@@ -20,6 +21,22 @@ userRouter.post("/save/:id", isAuthenticate, savePost);
 userRouter.put("/profile/update/:id", isAuthenticate, (req, res) => {
   res.send({ message: "updated profile" });
 });
+
+
+userRouter.put('/profile/dark/:id', isAuthenticate, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const found_user = await user.findById(id);
+    if (!found_user) {
+      res.status(400).json({ message: 'user not found' })
+    }
+    found_user.dark = !found_user.dark;
+    await found_user.save();
+    return res.status(200).json({ message: found_user })
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal Error' })
+  }
+})
 
 
 
